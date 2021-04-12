a471fd54b7ae341f78e8fe5c45558177be662c88
gmurro <g.murro23@gmail.com>
2021-04-12 15:14:16 +0200

    change coco

--- src/coco.py
+++ src/coco.py
@@ -1,32 +1,66 @@
 from pycocotools.coco import COCO
+import matplotlib.pyplot as plt
+import numpy as np
 import cv2
+from PIL import Image
 
 dataDir='..\\data\\train\\images'
 masksDir='..\\data\\train\\masks'
 
 coco = COCO("..\\data\\train\\annotations.json")
+img = Image.open("C:/Users/peppe/UNIBO/Deep Learning/lab/FoodRecognition/data/train/images/006316.jpg")
+plt.imshow(img)
+
+
+fig, ax = plt.subplots()
+ax.imshow(img)
+ax.spines['top'].set_visible(False)
+ax.spines['left'].set_visible(False)
+ax.spines['bottom'].set_visible(False)
+ax.spines['right'].set_visible(False)
+ax.set_xticks([])
+ax.set_yticks([])
+plt.show()
 
 cats = coco.loadCats(coco.getCatIds())
-nms=[cat['name'] for cat in cats]
+nms=[cat['id'] for cat in cats]
+#print(cats)
 
-print(nms)
-catIds = coco.getCatIds(catNms=nms);
 
-#print(catIds)
+catIds = coco.getCatIds();
 
-imgIds = coco.getImgIds(catIds=catIds );
+print(catIds)
 
-imgIds = coco.getImgIds(imgIds)
 
+#imgIds = coco.getImgIds(catIds=catIds );
+
+#imgIds = coco.getImgIds(imgIds)
+
+#imgs = coco.loadImgs(imgIds)
+
+#print(imgs)
+
+imgIds = coco.getImgIds()
+#print(imgIds)
 imgs = coco.loadImgs(imgIds)
 
 #print(imgs)
 
-imgs = coco.loadImgs(imgIds)
-
 #anns = coco.getAnnIds(imgIds=imgIds)
 #print(anns)
 
+img = imgs[0]
+print(img)
+ann = coco.getAnnIds(imgIds=img['id'])
+ann = coco.loadAnns(ann)[0]
+# print(ann)
+m = coco.annToMask(ann) #* 255
+print(np.unique(coco.annToMask(ann)))
+cv2.imwrite(masksDir + "/" + img["file_name"], m)
+plt.imshow(m)
+cv2.waitKey(0)
+
+"""
 for img in imgs:
     ann = coco.getAnnIds(imgIds=img['id'])
 
@@ -38,4 +72,4 @@ for img in imgs:
     #cv2.imshow("", m)
     #cv2.waitKey(0)
     
-    
+"""
---
+++ src/extract_segmentation.py
@@ -0,0 +1,22 @@
+from pycocotools.coco import COCO
+
+
+class ExtractSegmentations:
+
+    def __init__(self, imgDir, annotationPath, masksDir):
+        self.imgDir = imgDir
+        self.annotationPath = annotationPath
+        self.maskDir = masksDir
+
+        self.coco = COCO(annotationPath)
+
+
+    def getCats(self):
+        return self.coco.loadCats(self.coco.getCatIds())
+
+
+    def mapIdsToIndexes(self):
+        ids = self.coco.getCatIds()
+        idx = [i for i in range(ids)]
+        return idx
+
