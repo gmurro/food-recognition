8477ed63019b7dd50aff8735bb84c62dd5b65613
gmurro <g.murro23@gmail.com>
2021-04-26 17:08:57 +0200

    extract segmentation

--- src/coco.py
+++ src/coco.py
@@ -1,5 +1,8 @@
 from pycocotools.coco import COCO
 import matplotlib.pyplot as plt
+
+import matplotlib
+matplotlib.use("TkAgg")
 import numpy as np
 import cv2
 from PIL import Image
@@ -17,7 +20,7 @@ nms=[cat['id'] for cat in cats]
 
 
 catIds = coco.getCatIds();
-
+catIds.insert(0,0)
 print(len(catIds))
 
 
@@ -32,7 +35,14 @@ print(len(catIds))
 imgIds = coco.getImgIds()
 #print(imgIds)
 imgs = coco.loadImgs(imgIds)
-
+print(len(imgs))
+'''
+for img in imgs:
+    anns = coco.getAnnIds(imgIds=img['id'])
+    for ann in anns:
+        ann = coco.loadAnns(ann)[0]
+        print(ann['category_id'])
+'''
 #print(imgs)
 
 #anns = coco.getAnnIds(imgIds=imgIds)
@@ -44,14 +54,20 @@ print(img)
 original_image = cv2.imread(dataDir+"/"+img['file_name'])
 plt.imshow(cv2.cvtColor(original_image, cv2.COLOR_BGR2RGB))
 plt.show()
+original_image = np.array(original_image, dtype=np.uint16)
+cv2.imwrite("e.jpg",original_image)
 
+#print(original_image[0,0,:])
+
+'''
 anns = coco.getAnnIds(imgIds=img['id'])
 print(anns)
+
 
 mask = np.zeros(original_image.shape[:2])
 for ann in anns:
     ann = coco.loadAnns(ann)[0]
-    #print(ann)
+    print(ann)
     mask += coco.annToMask(ann)
     #print(np.unique(coco.annToMask(ann)))
 
@@ -61,6 +77,7 @@ for ann in anns:
     plt.show()
 
 print(np.unique(mask))
+'''
 """
 for img in imgs:
     ann = coco.getAnnIds(imgIds=img['id'])
@@ -74,3 +91,20 @@ for img in imgs:
     #cv2.waitKey(0)
     
 """
+
+'''
+def display_sample(display_list):
+    """Show side-by-side an input image,
+    the ground truth and the prediction.
+    """
+    plt.figure(figsize=(18, 18))
+
+    title = ['Input Image', 'True Mask', 'Predicted Mask']
+
+    for i in range(len(display_list)):
+        plt.subplot(1, len(display_list), i+1)
+        plt.title(title[i])
+        plt.imshow(tf.keras.preprocessing.image.array_to_img(display_list[i]))
+        plt.axis('off')
+    plt.show()
+'''
--- src/extract_segmentation.py
+++ src/extract_segmentation.py
@@ -1,24 +1,107 @@
 from pycocotools.coco import COCO
-
+import numpy as np
+import cv2
+import matplotlib.pyplot as plt
+import matplotlib
 
 class ExtractSegmentations:
 
-    def __init__(self, imgDir, annotationPath, masksDir):
+    def __init__(self, pathDir, imgDir='/images/', annotationFile='annotations.json', masksDir='masks/'):
+        self.pathDir = pathDir
         self.imgDir = imgDir
-        self.annotationPath = annotationPath
-        self.maskDir = masksDir
+        self.annotationFile = annotationFile
+        self.masksDir = masksDir
+        self.coco = COCO(pathDir+annotationFile)
 
-        self.coco = COCO(annotationPath)
+        # list of category ids adding 0 as first element
+        catIds = self.coco.getCatIds()
+        catIds.insert(0,0)
+        catIds.sort()
+        self.catIds = catIds
+        self.mapCatIdsLabels = dict(zip(catIds, [i for i in range(len(catIds))]))
 
 
-    def getCats(self):
-        return self.coco.loadCats(self.coco.getCatIds())
+
+    def intToRgb(self, value):
+        b = value % 256
+        g = ((value - b) / 256) % 256
+        r = ((value - b) / 256 ** 2) - g / 256
+        return np.array([r,g,b],dtype=np.uint8)
+
+    def rgbToInt(self, pixel):
+        r = pixel[0]
+        g = pixel[1]
+        b = pixel[2]
+        return 256**2 * r + 256 * g + b;
 
 
-    def mapIdsToIndexes(self):
-        ids = self.coco.getCatIds()
-        idx = [i for i in range(ids)]
-        return idx
+    def getMultiChannelMasks(self):
+        imgIds = self.coco.getImgIds()
+        imgs = self.coco.loadImgs(imgIds)
+
+        masks = list()
+
+        return [self.loadMask(img['id']) for img in imgs]
+
+
+
+    def loadMask(self, imgId):
+        """Generate instance masks for an image.
+       Returns:
+        masks: A bool array of shape [height, width, instance count] with
+            one mask per instance.
+        class_ids: a 1D array of class IDs of the instance masks.
+        """
+
+        img = self.coco.loadImgs(imgId)[0]
+
+        # load all annotations for the given image
+        anns = self.coco.getAnnIds(imgIds=imgId)
+        instance_count = len(anns)
+
+
+        # array of class IDs of each instance
+        catIds = list()
+
+        # Convert annotations to a bitmap mask of shape [height, width, instance_count]
+        mask = np.zeros((img['height'], img['width'], instance_count), dtype=np.uint8)
+
+        for i, ann in enumerate(anns):
+            ann = self.coco.loadAnns(ann)[0]
+            mask[:, :, i] = np.array(self.coco.annToMask(ann), dtype=np.uint8)
+
+            catIds.append(ann['category_id'])
+
+        # Return mask, and array of class IDs of each instance. Since we have
+        # one class ID only, we return an array of 1s
+        # Map class names to class IDs.
+        return mask, catIds
+
+
+
+    def extractMasks(self):
+        imgIds = self.coco.getImgIds()
+        imgs = self.coco.loadImgs(imgIds)
+
+        masks = list()
+
+        for img in imgs:
+            anns = self.coco.getAnnIds(imgIds=img['id'])
+
+            mask = np.zeros((img['height'], img['width']), dtype=np.uint16)
+            for ann in anns:
+                ann = self.coco.loadAnns(ann)[0]
+                catId = ann['category_id']
+                mask += (np.array(self.coco.annToMask(ann), dtype=np.uint16)*catId)
+
+            cv2.imwrite(self.pathDir + self.masksDir + img["file_name"], mask)
+            masks.append(mask)
+        return masks
+
+
+
+
+
 
 
 
