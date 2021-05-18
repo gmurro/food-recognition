1d7ba38d0426dcb87f9f5d79fbc6e06900339459
SalvoPisciotta <salvopisciotta97@gmail.com>
2021-05-18 18:49:10 +0200

    Implemented function of augmentation and imported the required libraries

--- src/utils/segmentation_utils.py
+++ src/utils/segmentation_utils.py
@@ -1,15 +1,18 @@
+
 from pycocotools.coco import COCO
 import numpy as np
 import cv2
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 from tqdm import tqdm
+from random import random
 import os
 from skimage.transform import resize
 
 from tensorflow.keras.preprocessing.image import load_img
 from tensorflow.keras.preprocessing.image import img_to_array
 from tensorflow.keras.utils import Progbar
+from keras.preprocessing.image import ImageDataGenerator
 
 
 def load_coco_dataset(pathDir, annotationFile='annotations.json'):
@@ -86,7 +89,7 @@ def load_imgs(coco, cat_names=[]):
 
     # get cat_names ordered and with the special category 'background'
     cats = coco.loadCats(cat_ids)
-    cat_names = ['background']+[cat['name'] for cat in cats]
+    cat_names = ['background' ] +[cat['name'] for cat in cats]
 
     # build a map from cat_ids to to the corresponded index of cat_names
     cat_map = dict(zip([0] + cat_ids, range(len(cat_names))))
@@ -152,5 +155,88 @@ def load_mask(coco, img_id, cat_map):
     return mask
 
 
+def get_augmented(X, y, image_size, num_classes, batch_size=16, index=-1):
+    """
+  Generative function that create a batch of augumented images and masks
+  for the passed set of images and their correspondent masks.
 
+  Parameters
+  ----------
+  X :nparray
+      set of images
+  y :nparray
+        set of masks with index equal to the one of the corresponding image
+  image_size : tuple of int size 2
+         size of the image and mask, first parameter is the weight, the second is the height
+  num_classes: int
+         number of categories for the segmentation
+  batch_size: int
+         size of the batch created for each image
+  index: int
+         index of the image to generate the batch, if not present it will choice randomly
+  Returns
+  -------
+    Two np.array, one that represent the batch of modified images with shape [batch,sizeheight, width, n_categories]
+    and the other one that represent the batch modified masks with shape [batch,sizeheight, width, n_categories]
+    where last channel is the binary mask correspondent to each category.
+  """
+    # the arguments used by keras ImageGenerator for images
+    aug_generator_args = dict(featurewise_center=False,
+                              samplewise_center=False,
+                              rotation_range=5,
+                              width_shift_range=0.01,
+                              height_shift_range=0.01,
+                              brightness_range=(0.8, 1.2),
+                              shear_range=0.01,
+                              zoom_range=[1, 1.25],
+                              horizontal_flip=True,
+                              vertical_flip=False,
+                              fill_mode='reflect',
+                              data_format='channels_last')
 
+    aug_generator_args_mask = aug_generator_args.copy()
+    # the arguments used by keras ImageGenerator for mask (same but without brightness)
+    aug_generator_args_mask.pop('brightness_range' ,None)
+
+    image_gen = ImageDataGenerator(**aug_generator_args)
+    mask_gen = ImageDataGenerator(**aug_generator_args_mask)
+
+    while True:
+
+        if index == -1:
+            idx = np.random.choice(len(X - 1))
+        else:
+            idx = index
+
+        image = X[idx]
+        mask = y[idx]
+        print(mask.shape)
+        valid_mask = []
+        for i in range(num_classes):
+            channel = mask[:, :, i]
+            if channel.any():
+                valid_mask.append(channel)
+
+        X_aug = np.zeros((batch_size, image_size[0], image_size[1], 3)).astype('float')
+        y_aug = np.zeros((batch_size, image_size[0], image_size[1], num_classes)).astype('float')
+
+        seed = np.random.choice(range(9999))
+
+        g_x = image_gen.flow(image.reshape((1,) + image.shape),
+                             batch_size=batch_size,
+                             seed=seed)
+
+        g_ys = []
+
+        for i in range(len(valid_mask)):
+            g_ys.append(mask_gen.flow(valid_mask[i].reshape((1,) + valid_mask[i].shape + (1,)),
+                                      batch_size=batch_size,
+                                      seed=seed))
+
+        for batch_num in range(batch_size):
+            X_aug[batch_num] = g_x.next() / 255.0
+            for i in range(len(valid_mask)):
+                cat_channel = g_ys[i].next().reshape([160, 160])
+                y_aug[batch_num, :, :, i] = cat_channel
+
+        yield X_aug, y_aug
