1ef970f159a5dd5ad3e1b7344a8e02e81298efab
gmurro <g.murro23@gmail.com>
2021-05-22 01:00:45 +0200

    fix data augmentation

--- src/utils/segmentation_utils.py
+++ src/utils/segmentation_utils.py
@@ -1,13 +1,10 @@
-
 from pycocotools.coco import COCO
 import numpy as np
-import cv2
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
-from tqdm import tqdm
-from random import random
+import matplotlib.patches as mpatches
 import os
-from skimage.transform import resize
+from PIL import Image
 
 from tensorflow.keras.preprocessing.image import load_img
 from tensorflow.keras.preprocessing.image import img_to_array
@@ -15,50 +12,87 @@ from tensorflow.keras.utils import Progbar
 from keras.preprocessing.image import ImageDataGenerator
 
 
-def load_coco_dataset(pathDir, annotationFile='annotations.json'):
-    return COCO(os.path.join(pathDir, annotationFile))
+def load_coco_dataset(path_dir, annotation_file='annotations.json'):
+    """
+    Load COCO object reading info about the COCO annotations
+
+    Parameters
+    ----------
+    path_dir : str
+        The directory containing the annotations file
+    annotation_file : str
+        Filename of the annotations file
+
+    Returns
+    -------
+    object
+        COCO object containing info about the COCO annotations
+    """
+    return COCO(os.path.join(path_dir, annotation_file))
 
 
-def augmentation(images, masks):
-    return None
-    # return augmented (images, masks)
+def load_data(coco, img_dir, img_size, cat_names=[],  show_progress=True):
+    """
+    Load the whole dataset in one shot, filtering it for some categories
 
+    Parameters
+    ----------
+    coco: object
+        COCO object containing info about the COCO annotations
+    img_dir: str
+        The path to the directory containing images to read
+    img_size: tuple
+        Height and width according to resize the images
+    cat_names: list, optional
+        List of categories according to filter the dataset. If empty, load all dataset
+    show_progress: bool, optional
+        If True, it allows to show the progress of the loading
 
-def load_data(coco, imgDir, img_size, cat_names=[],  show_progress=True):
+    Returns
+    ----------
+    tuple
+        (x, y) where the first element is a np.array of input image
+        and the second element is a np.array of binary masks
+    """
 
     # load images info from annotations
-    img_ids, img_names, cat_names, cat_map = load_imgs(coco, cat_names)
+    img_ids, img_names, cat_names, cat_to_label = load_imgs(coco, cat_names)
 
     # prepare structure of input images
-    x = np.zeros((len(img_names),) + img_size + (3,), dtype="float32")
+    x = np.zeros((len(img_names),) + img_size + (3,), dtype=np.float32)
 
     if show_progress:
         print("Loading images:")
-        pbar = Progbar(target=len(img_names))
+        bar_img = Progbar(target=len(img_names))
 
     for i, filename in enumerate(img_names):
-        path = os.path.join(imgDir, filename)
+        path = os.path.join(img_dir, filename)
         img = img_to_array(load_img(path, target_size=img_size))
         x[i] = img
 
         if show_progress:
-            pbar.update(i)
+            bar_img.update(i)
 
     # prepare structure of target masks
-    y = np.zeros((len(img_names),) + img_size + (len(cat_names),), dtype="float32")
+    y = np.zeros((len(img_names),) + img_size + (len(cat_names),), dtype=np.float32)
 
     if show_progress:
-        print("Loading masks:")
-        pbar = Progbar(target=len(img_names))
+        print("\nLoading masks:")
+        bar_mask = Progbar(target=len(img_ids))
 
     for i, img_id in enumerate(img_ids):
+        mask = load_mask(coco, img_id, cat_to_label)
 
-        mask = load_mask(coco, img_id, cat_map)
-        # TODO resize mask
-        y[i] = mask
+        # scale down mask
+        mask_scaled = np.zeros(img_size + (len(cat_names),), dtype=np.uint8)
+        for j in range(len(cat_names)):
+            mask_scaled[:, :, j] = Image.fromarray(mask[:, :, j]).resize(img_size, Image.NEAREST)
+
+        y[i] = np.array(mask_scaled, dtype=np.float32)
+
         if show_progress:
-            pbar.update(i)
-    return x
+            bar_mask.update(i)
+    return x, y
 
 
 def load_imgs(coco, cat_names=[]):
@@ -89,20 +123,20 @@ def load_imgs(coco, cat_names=[]):
 
     # get cat_names ordered and with the special category 'background'
     cats = coco.loadCats(cat_ids)
-    cat_names = ['background' ] +[cat['name'] for cat in cats]
+    cat_names = ['background'] +[cat['name'] for cat in cats]
 
     # build a map from cat_ids to to the corresponded index of cat_names
-    cat_map = dict(zip([0] + cat_ids, range(len(cat_names))))
+    cat_to_label = dict(zip([0] + cat_ids, range(len(cat_names))))
 
     # get images annotated with the filtered categories
     img_ids = [coco.getImgIds(catIds=[id]) for id in cat_ids]
     img_ids = [item for sublist in img_ids for item in sublist]  # List of lists flattening
     img_names = [coco.loadImgs(img_id)[0]['file_name'] for img_id in img_ids]
 
-    return img_ids, img_names, cat_names, cat_map
+    return img_ids, img_names, cat_names, cat_to_label
 
 
-def load_mask(coco, img_id, cat_map):
+def load_mask(coco, img_id, cat_to_label):
     """
     Generate a multichannel mask for an image.
 
@@ -112,7 +146,7 @@ def load_mask(coco, img_id, cat_map):
         coco object containing info about the COCO annotations
     img_id: int
         id of the image for which you want get the mask
-    cat_map: dict
+    cat_to_label: dict
         Map from cat_ids to to the corresponded index of cat_names
 
     Returns
@@ -127,7 +161,7 @@ def load_mask(coco, img_id, cat_map):
     anns = coco.getAnnIds(imgIds=img_id)
 
     # number of categories according to generate the mask. The background must be included
-    n_categories = len(cat_map)
+    n_categories = len(cat_to_label)
 
     # Convert annotations to a binary mask of shape [height, width, n_categories]
     mask = np.zeros((img['height'], img['width'], n_categories), dtype=np.float32)
@@ -139,10 +173,10 @@ def load_mask(coco, img_id, cat_map):
         ann = coco.loadAnns(ann)[0]
 
         # check if it is a considered category
-        if ann['category_id'] in list(cat_map.keys()):
+        if ann['category_id'] in list(cat_to_label.keys()):
 
             # get channel to fill
-            i = cat_map[ann['category_id']]
+            i = cat_to_label[ann['category_id']]
             mask[:, :, i] = np.array(coco.annToMask(ann), dtype=np.float32)
 
             # compose incrementally the background channel
@@ -154,6 +188,153 @@ def load_mask(coco, img_id, cat_map):
 
     return mask
 
+
+def augment(x, y, img_aug_args):
+    """
+    Generative function that create a batch of augmented images and masks
+    for the passed set of images and their correspondent masks.
+
+    Parameters
+    ----------
+    x : np.array
+        Batch of input images
+    y : np.array
+        Batch of masks
+    img_aug_args: dict
+        Parameters of the Keras ImageDataGenerator class
+
+    Returns
+    ----------
+    tuple
+        (x, y) of the same shape of the input ones, but transformed
+    """
+    mask_aug_args = img_aug_args.copy()
+
+    # the brightness parameter is unuseful for the mask, remove it
+    if mask_aug_args.keys().__contains__('brightness_range'):
+        mask_aug_args.pop('brightness_range', None)
+
+    # we apply data augmentation to our datasets using Keras ImageDataGenerator class
+    image_aug = ImageDataGenerator(**img_aug_args)
+    mask_aug = ImageDataGenerator(**mask_aug_args)
+
+    # transpose the batch of the masks to iter over the channels
+    y_t = y.transpose((3, 0, 1, 2))
+
+    # prepare data structure for the augmentation result
+    x_aug = np.zeros(x.shape, dtype=np.float32)
+    y_aug = np.zeros(y_t.shape, dtype=np.float32)
+
+    # seed useful to sync the augmentation fof x and y applying the same transformation
+    seed = np.random.choice(range(9999))
+
+    batch_size = x.shape[0]
+    n_channel = y.shape[3]
+
+    # create a generator that can return an augmented batch of images
+    image_gen = image_aug.flow(x, batch_size=batch_size, shuffle=False, seed=seed)
+
+    mask_gen = []
+    for i in range(n_channel):
+        # create a generator that can return an augmented batch of masks
+        mask_gen.append(mask_aug.flow(y_t[i].reshape(y_t[i].shape + (1,)), shuffle=False, batch_size=batch_size, seed=seed))
+
+    # get the augmented batch
+    x_aug = image_gen.next()
+    for i in range(n_channel):
+        y_aug[i] = mask_gen[i].next()[:,:,:,0]
+
+    return x_aug, y_aug.transpose((1, 2, 3, 0))
+
+
+def show_im(img_path, img=None):
+    """
+    Auxiliary function to plot images.
+
+    Parameters
+    ----------
+    img_path : str
+      The file location from which read the image. It is ignored if you pass img.
+    img : np.array, optional
+      If you have a np.array object in a form of image, you can pass it direcly.
+    """
+
+    fig = plt.figsize=(10,)
+
+    # read image from the defined path if img it is available
+    if img is None:
+        img = mpimg.imread(img_path)
+
+    plt.imshow(img)
+    plt.axis('off')
+    plt.show()
+
+
+def show_multiple_im(input_img_paths, target_img_paths, cat_names, predictions=None, figsize=(10, 10)):
+    """
+    Auxiliary function to plot a batch of images.
+
+    Parameters
+    ----------
+    input_img_paths : str
+        The file locations from which read the input images.
+    target_img_paths : str
+        The file locations from which read the masks.
+    cat_names : list
+        Array of the categories to show on segmentation masks
+    predictions : np.array, optional
+        Batch of predictions.
+    figsize :  array
+        Size of the figure to plot.
+    """
+
+    n_images = len(input_img_paths)
+
+    # plot 3 or 2 columns if predictions is available or not
+    ncols = 2 if predictions is None else 3
+
+    fig, axes = plt.subplots(figsize=figsize, nrows=n_images, ncols=ncols)
+
+    for i, (input_img_path, target_img_path) in enumerate(zip(input_img_paths, target_img_paths)):
+        img = mpimg.imread(input_img_path)
+        ax = axes[i, 0] if n_images > 1 else axes[0]
+        ax.imshow(img)
+        ax.axis('off')
+
+        mask = img_to_array(load_img(target_img_path, color_mode="grayscale"))
+        ax = axes[i, 1] if n_images > 1 else axes[1]
+        im = ax.imshow(mask[:, :, 0])
+        ax.axis('off')
+
+        values = np.array(np.unique(mask), dtype=np.uint8)
+        colors = [im.cmap(im.norm(value)) for value in range(len(cat_names))]
+        patches = [mpatches.Patch(color=colors[i], label=cat_names[i]) for i in values]
+        ax.legend(handles=patches, bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0.)
+
+        if predictions is not None:
+            ax = axes[i, 2] if n_images > 1 else axes[2]
+            ax.imshow(predictions[i])
+            ax.axis('off')
+
+            values = np.array(np.unique(predictions[i]), dtype=np.uint8)
+            colors = [im.cmap(im.norm(value)) for value in range(len(cat_names))]
+            patches = [mpatches.Patch(color=colors[i], label=cat_names[i]) for i in values]
+            ax.legend(handles=patches, bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0.)
+
+    # set column title
+    col_titles = ['Input', 'Target', 'Predictions']
+    if n_images > 1:
+        for ax, col in zip(axes[0], col_titles):
+            ax.set_title(col)
+    else:
+        for ax, col in zip(axes, col_titles):
+            ax.set_title(col)
+
+    fig.tight_layout()
+    plt.show()
+
+'''
+# TODO REMOVE THIS 
 
 def get_augmented(X, y, image_size, num_classes, batch_size=16, index=-1):
     """
@@ -174,6 +355,7 @@ def get_augmented(X, y, image_size, num_classes, batch_size=16, index=-1):
          size of the batch created for each image
   index: int
          index of the image to generate the batch, if not present it will choice randomly
+
   Returns
   -------
     Two np.array, one that represent the batch of modified images with shape [batch,sizeheight, width, n_categories]
@@ -181,17 +363,15 @@ def get_augmented(X, y, image_size, num_classes, batch_size=16, index=-1):
     where last channel is the binary mask correspondent to each category.
   """
     # the arguments used by keras ImageGenerator for images
-    aug_generator_args = dict(featurewise_center=False,
-                              samplewise_center=False,
-                              rotation_range=5,
+    aug_generator_args = dict(rotation_range=10,
                               width_shift_range=0.01,
                               height_shift_range=0.01,
                               brightness_range=(0.8, 1.2),
                               shear_range=0.01,
-                              zoom_range=[1, 1.25],
+                              zoom_range=[1, 1.5],
                               horizontal_flip=True,
-                              vertical_flip=False,
-                              fill_mode='reflect',
+                              vertical_flip=True,
+                              fill_mode='constant',
                               data_format='channels_last')
 
     aug_generator_args_mask = aug_generator_args.copy()
@@ -240,3 +420,4 @@ def get_augmented(X, y, image_size, num_classes, batch_size=16, index=-1):
                 y_aug[batch_num, :, :, i] = cat_channel
 
         yield X_aug, y_aug
+'''
